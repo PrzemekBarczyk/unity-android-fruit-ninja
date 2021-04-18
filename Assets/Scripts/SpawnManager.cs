3c7835e2e45d7b5d54694de6d48bb27842fd5200
PrzemekBarczyk <przemek.barczyk@gmail.com>
2021-04-18 10:16:41 +0200

    Spawn Manager
    Creates new targets within random rate range.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] targetPrefabs;
+    [SerializeField] float minSpawnRate;
+    [SerializeField] float maxSpawnRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+	{
+        while (true)
+		{
+            yield return new WaitForSeconds(Random.Range(minSpawnRate, maxSpawnRate));
+            int randomTargetIndex = Random.Range(0, targetPrefabs.Length);
+            Instantiate(targetPrefabs[randomTargetIndex], transform.position, transform.rotation);
+        }
+	}
+}
